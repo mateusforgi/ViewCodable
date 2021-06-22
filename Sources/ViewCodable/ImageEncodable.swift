d2d76e85ab11723f3fc780e827e6d89988f09be3
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2021-06-21 21:40:17 -0300

    FEAT - Adicionar Image codable.

--- Sources/ViewCodable/ImageEncodable.swift
+++ Sources/ViewCodable/ImageEncodable.swift
@@ -5,12 +5,55 @@
 //  Created by Mateus Forgiarini on 10/26/20.
 //
 
-import Foundation
+import SwiftUI
+import Combine
 
-public struct ImageEncodable: Encodable {
+public struct ImageCodable: Codable {
     public var url: String
 
     public init(url: String) {
         self.url = url
+    }
+    
+    public var body: some View {
+        ImageView(withURL: url)
+    }
+}
+
+private class ImageLoader: ObservableObject {
+    var didChange = PassthroughSubject<Data, Never>()
+    var data = Data() {
+        didSet {
+            didChange.send(data)
+        }
+    }
+    
+    init(urlString:String) {
+        guard let url = URL(string: urlString) else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            DispatchQueue.main.async {
+                self.data = data
+            }
+        }
+        task.resume()
+    }
+}
+
+private struct ImageView: View {
+    @ObservedObject var imageLoader:ImageLoader
+    @State var image: NSImage = NSImage()
+    
+    init(withURL url:String) {
+        imageLoader = ImageLoader(urlString:url)
+    }
+    
+    var body: some View {
+        Image(nsImage: image)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .onReceive(imageLoader.didChange) { data in
+                self.image = NSImage(data: data) ?? NSImage()
+            }
     }
 }
