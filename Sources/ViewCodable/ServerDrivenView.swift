f75f3e43a3d6340e3b0e825448869bfa8d872130
Mateus Forgiarini da Silva <mateusforgi@gmail.com>
2021-07-05 22:08:52 -0300

    FEAT - Add default values for ServerDrivenView

--- Sources/ViewCodable/ServerDrivenView.swift
+++ Sources/ViewCodable/ServerDrivenView.swift
@@ -11,3 +11,13 @@ public protocol ServerDrivenView: View, Codable {
     var destination: String? { get }
     var navigationTitle: String? { get }
 }
+
+public extension ServerDrivenView {
+    var destination: String? {
+        return nil
+    }
+    
+    var navigationTitle: String? {
+        return nil
+    }
+}
